c2694ebc286a13a13ffc99351fb65184d1b9d7ed
Ishiduka Kai <i2307lqq@sit.ac.jp>
2024-12-10 15:48:49 +0900

    適当なコメントを追加

---
+++ load_dataset.py
@@ -0,0 +1,24 @@
+from torchvision import datasets
+import matplotlib.pyplot as plt
+import torchvision.transforms.v2 as transforms
+import torch
+
+ds_train = datasets.FashionMNIST(
+    root='data',
+    train=True,
+    download=True,
+)
+
+print(len(ds_train))  
+
+image, target = ds_train[0]
+print(type(image), target)
+
+plt.imshow(image)
+plt.title(target)
+plt.show()
+
+image = transforms.functional.to_image(image)
+image = transforms.functional.to_dtype(image, dtype=torch.float32, scale=True)
+print(image.shape, image.dtype)
+print(image,min(), image.max())
